2741319318d8bc0100104198d5013373d72e7beb
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2023-06-16 12:08:11 +0200

    assest organized and cards almost af

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+var cardSection = document.querySelector('.card-section')
+var nextBtn = document.querySelector ('.next-btn')
+var prevBtn = document.querySelector ('.prev-btn')
 
+
+nextBtn.onclick = () => {
+    cardSection.scrollLeft += 500;
+  };
+  prevBtn.onclick = () => {
+    cardSection.scrollLeft -= 500;
+  };
 
