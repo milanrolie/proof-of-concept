8d651b43472f3983208dc5475e6ef8fad5e9b522
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-06-19 08:31:20 +0200

    Revert "assest organized and cards almost af"
    This reverts commit 2741319318d8bc0100104198d5013373d72e7beb.

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-var cardSection = document.querySelector('.card-section')
-var nextBtn = document.querySelector ('.next-btn')
-var prevBtn = document.querySelector ('.prev-btn')
 
-
-nextBtn.onclick = () => {
-    cardSection.scrollLeft += 500;
-  };
-  prevBtn.onclick = () => {
-    cardSection.scrollLeft -= 500;
-  };
 
