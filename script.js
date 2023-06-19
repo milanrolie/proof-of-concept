a145a1453523bcf10f6fe9542dba98ce9b3e327e
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2023-06-19 10:58:31 +0200

    prismic connectie

--- script.js
+++ script.js
@@ -3,6 +3,32 @@ var nextBtn = document.querySelector ('.next-btn')
 var prevBtn = document.querySelector ('.prev-btn')
 
 
+import * as prismic from 'https://cdn.skypack.dev/@prismicio/client';
+
+const repositoryName = 'projectink'
+const accessToken = 'MC5aSTNldEJVQUFDb0FCNFFt.dSHvv71CND8t77-977-977-9Mzp377-9fjTvv73vv73vv73vv73vv73vv73vv71G77-977-9GO-_ve-_vWrvv718' // Set an access token
+
+
+const client = prismic.createClient(repositoryName, { accessToken })
+
+const init = async () => {
+  const players = await client.getAllByType('spelers')
+  
+  players.forEach(player => {
+    
+
+    console.log(player.data)
+
+    
+  });
+
+ 
+}
+
+init()
+
+
+//card carousel buttons functionality. als er op de knop geklikt wordt schuift de scrollbar met 500 pixels
 nextBtn.onclick = () => {
     cardSection.scrollLeft += 500;
   };
